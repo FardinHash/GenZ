ae874531907b8c5f67be84812f561c22d0cc45aa
FardinHash <fardin@iiec.iubat.edu>
2025-08-12 14:59:28 +0600

    FEAT: integrate Sentry for Next.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,4 +6,6 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig; 
+const { withSentryConfig } = require('@sentry/nextjs');
+
+module.exports = withSentryConfig(nextConfig, { silent: true }); 
